4f4e356c532b4575d9d22d16d34790564924a3af
Erick-Bravo <erickbrvo@gmail.com>
2023-10-14 21:23:18 -0700

    redux budget update

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,12 +2,27 @@ import React, { useContext, useState } from "react";
 import { AppContext } from "../context/AppContext";
 
 const Budget = () => {
-  const { budget } = useContext(AppContext);
+  const { budget, expenses, dispatch } = useContext(AppContext);
   const [newBudget, setNewBudget] = useState(budget);
+  console.log(budget)
+
+  const totalExpenses = expenses.reduce((total, item) => {
+    return (total = total + item.cost);
+  }, 0);
+
   const handleBudgetChange = (event) => {
-    setNewBudget(event.target.value);
+
+    if (budget < totalExpenses) {
+      alert("You cannot reduce the budget value lower than the spending");
+    } else {
+      setNewBudget(event.target.value);
+      dispatch({
+        type: "SET_BUDGET",
+        payload: newBudget
+      })
+    }
   };
-  
+
   return (
     <div className="alert alert-secondary">
       <span>Budget: £{budget}</span>
