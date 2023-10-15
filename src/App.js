5f81d75ff95716148f4a346d32e5f0831e73f17d
Erick-Bravo <erickbrvo@gmail.com>
2023-10-14 20:19:10 -0700

    Expense list update

--- src/App.js
+++ src/App.js
@@ -19,31 +19,30 @@ const App = () => {
       <div className="container">
         <h1 className="mt-3">Company's Budget Allocation</h1>
         <div className="row mt-3">
-      
           <div className="col-sm">
             <Budget />
           </div>
-     
+
           <div className="col-sm">
             <Remaining />
           </div>
-     
+
           <div className="col-sm">
             <ExpenseTotal />
           </div>
-          <h3 className="mt-3">Allocation</h3>
-          <div className="row">
-            <div className="col-sm">
-              <ExpenseList />
-            </div>
+        </div>
+        <h3 className="mt-3">Allocation</h3>
+        <div className="row">
+          <div className="col-sm">
+            <ExpenseList />
           </div>
+        </div>
 
-          <h3 className='mt-3'>Change allocation</h3>
-          <div className='row mt-3'>
-                    <div className='col-sm'>
-                        <AllocationForm/>
-                    </div>
-                </div>
+        <h3 className="mt-3">Change allocation</h3>
+        <div className="row mt-3">
+          <div className="col-sm">
+            <AllocationForm />
+          </div>
         </div>
       </div>
     </AppProvider>
